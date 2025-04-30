65582a29cb0d76ade7c064a840a21da178f4e63a
NESLITDEV <mubir.neslit@gmail.com>
2025-05-01 00:03:08 +0500

    Add new POWER-LOGO assets and update SidebarHeader to use the new logo - Added POWER-LOGO.jpg and POWER-LOGO.svg to the media directory. - Updated SidebarHeader component to replace default logo references with the new POWER-LOGO.jpg for both light and dark themes.

--- src/layouts/demo1/sidebar/SidebarHeader.jsx
+++ src/layouts/demo1/sidebar/SidebarHeader.jsx
@@ -9,16 +9,16 @@ const SidebarHeader = forwardRef((props, ref) => {
   } = useDemo1Layout();
   const lightLogo = () => <Fragment>
       <Link to="/" className="dark:hidden">
-        <img src={toAbsoluteUrl('/media/app/default-logo.svg')} className="default-logo min-h-[22px] max-w-none" />
+        <img src={toAbsoluteUrl('/media/POWER-LOGO.jpg')} className="default-logo min-h-[22px] max-w-[200px] px-10 py-10" />
         <img src={toAbsoluteUrl('/media/app/mini-logo.svg')} className="small-logo min-h-[22px] max-w-none" />
       </Link>
       <Link to="/" className="hidden dark:block">
-        <img src={toAbsoluteUrl('/media/app/default-logo-dark.svg')} className="default-logo min-h-[22px] max-w-none" />
+        <img src={toAbsoluteUrl('/media/POWER-LOGO.jpg')} className="default-logo min-h-[22px] max-w-[200px] px-10 py-10" />
         <img src={toAbsoluteUrl('/media/app/mini-logo.svg')} className="small-logo min-h-[22px] max-w-none" />
       </Link>
     </Fragment>;
   const darkLogo = () => <Link to="/">
-      <img src={toAbsoluteUrl('/media/app/default-logo-dark.svg')} className="default-logo min-h-[22px] max-w-none" />
+      <img src={toAbsoluteUrl('/media/POWER-LOGO.jpg')} className="default-logo min-h-[22px] max-w-[200px] px-10 py-10" />
       <img src={toAbsoluteUrl('/media/app/mini-logo.svg')} className="small-logo min-h-[22px] max-w-none" />
     </Link>;
   return <div ref={ref} className="sidebar-header hidden lg:flex items-center relative justify-between px-3 lg:px-6 shrink-0">
